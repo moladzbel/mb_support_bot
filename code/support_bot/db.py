66b748758e17053d68815e8c4203811692d51a76
Michaś Varantsou <meequz@gmail.com>
2025-01-14 15:09:53 +0100

    Report subject change

--- code/support_bot/db.py
+++ code/support_bot/db.py
@@ -25,7 +25,7 @@ class TgUsers(Base):
     username = sa.Column(sa.String(32))
     thread_id = sa.Column(sa.Integer, index=True)
     last_user_msg_at = sa.Column(sa.DateTime)
-    subject = sa.Column(sa.String(32))
+    subject = sa.Column(sa.String(63))
 
     banned = sa.Column(sa.Boolean, default=False, nullable=False)
     shadow_banned = sa.Column(sa.Boolean, default=False, nullable=False)
